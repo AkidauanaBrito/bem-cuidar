021020b529621554bc6154ce99a671f0be0abd6b
Akidauana Brito <akidauana.brito@gmail.com>
2024-11-23 12:13:03 -0300

    adicionando toast para ser exibido no cadastro de cuidador

--- src/components/cuidadores/Cuidadores.js
+++ src/components/cuidadores/Cuidadores.js
@@ -1,78 +1,89 @@
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import api from '../../api/axiosConfig';
 import { Container, Row, Col } from "react-bootstrap";
 import CuidadorForm from "../cuidador/CuidadorForm";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import React from "react";
 
-const Cuidadores = () =>{
+const Cuidadores = () => {
     const nomeText = useRef();
-
     const emailText = useRef();
     const passwordText = useRef();
     const cpfText = useRef();
 
-    const addCuidador = async (e) =>{
+    const addCuidador = async (e) => {
         e.preventDefault();
 
         const nome = nomeText.current;
         const email = emailText.current;
         const password = passwordText.current;
         const cpf = cpfText.current;
-        try{
-            const response = await api.post("api/v1/cuidadores",
-                {
-                    nome:nome.value,
-                    email:email.value,
-                    password:password.value,
-                    cpf:cpf.value
-                });
-    
-            //const updatedReviews = [...reviews,{body:rev.value}];
-    
-             nome.value  = "";
-             email.value  = "";
-             password.value  = "";
-             cpf.value  = "";
-             //setReviews(updatedReviews);
 
-        }catch(err){
-            console.log(err);
+        try {
+            await api.post("api/v1/cuidadores", {
+                nome: nome.value,
+                email: email.value,
+                password: password.value,
+                cpf: cpf.value,
+            });
+
+            toast.success("Cuidador cadastrado com sucesso!", {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+            });
+
+            nome.value = "";
+            email.value = "";
+            password.value = "";
+            cpf.value = "";
+        } catch (err) {
+            console.error(err);
+            toast.error("Erro ao cadastrar o cuidador.", {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+            });
         }
-    }
-    return (
+    };
 
+    return (
         <Container>
             <Row>
                 <Col><h3>Cadastro Cuidador</h3></Col>
             </Row>
             <Row>
                 <Col>
-                {
-                    <>
                     <Row>
                         <Col>
-                            <CuidadorForm 
-                            handleSubmit={addCuidador} 
-                            nomeText={nomeText}
-                            emailText={emailText}
-                            passwordText={passwordText}
-                            cpfText={cpfText} 
-                            labelText="Escreva um review"></CuidadorForm>
+                            <CuidadorForm
+                                handleSubmit={addCuidador}
+                                nomeText={nomeText}
+                                emailText={emailText}
+                                passwordText={passwordText}
+                                cpfText={cpfText}
+                                labelText="Escreva um review"
+                            />
                         </Col>
                     </Row>
                     <Row>
                         <Col>
-                            <hr/>
+                            <hr />
                         </Col>
                     </Row>
-                    </>
-                }
                 </Col>
             </Row>
-
+            <ToastContainer />
         </Container>
-    )
-}
+    );
+};
 
-export default Cuidadores
+export default Cuidadores;
