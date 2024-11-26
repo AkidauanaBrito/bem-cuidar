8b874168e5bd0476b361a1738fe3e8747abc5a5d
Akidauana Brito <akidauana.brito@gmail.com>
2024-11-26 19:28:21 -0300

    adicionando background para pagina home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,26 @@
 import React from "react";
 
-const Home = () =>{
-    return (
-        <div>
-            Welcome!
-        </div>
-    )
-}
+const Home = () => {
+    const backgroundImage = "url('/bemcuidar2.webp')";
 
-export default Home
+    return (
+        <div
+            style={{
+                backgroundImage: backgroundImage,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                height: '100vh',
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                fontSize: '3em',
+                color: 'white',
+            }}
+        >
+            Bem Vindo ao Bem Cuidar!
+        </div>
+    );
+};
+
+export default Home;
